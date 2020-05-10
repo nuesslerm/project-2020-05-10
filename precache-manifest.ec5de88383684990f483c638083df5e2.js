f46e583bcaf2211621c80ec83a1070af4a617ec9
nuesslerm <markus@choco.com>
2020-05-10 23:54:16 +0200

    Updates

---
+++ precache-manifest.ec5de88383684990f483c638083df5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4263391e9556b10c3784282d53206c",
+    "url": "/project-2020-05-10/index.html"
+  },
+  {
+    "revision": "7d0904db896b1961fdbe",
+    "url": "/project-2020-05-10/static/css/main.5a991a3f.chunk.css"
+  },
+  {
+    "revision": "55c696f00c00c5b9b3ff",
+    "url": "/project-2020-05-10/static/js/2.56afb2e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-2020-05-10/static/js/2.56afb2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0904db896b1961fdbe",
+    "url": "/project-2020-05-10/static/js/main.2bcdf942.chunk.js"
+  },
+  {
+    "revision": "634388ddb4bad9ff6bad",
+    "url": "/project-2020-05-10/static/js/runtime-main.960cf9a5.js"
+  }
+]);
